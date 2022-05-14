e889b3ee5544ff8f5bbb52805f7f81be03cb4d2e
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-14 22:43:32 +0300

    ft:added classes for all required forms

---
+++ app/main/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField,SubmitField, SelectField)
+from wtforms.validators import DataRequired
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+class UpdatePostForm(FlaskForm):
+    title = StringField("Blog title", validators=[DataRequired()])
+    post = TextAreaField("Type Away", validators=[DataRequired()])
+    submit = SubmitField("Update")
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField("Post Comment", validators=[DataRequired()])
+    alias = StringField("Comment Alias")
+    submit = SubmitField("Comment")
+
+class UpdateProfile(FlaskForm):
+    first_name = StringField("First name")
+    last_name = StringField("Last Name")
+    bio = TextAreaField("Bio")
+    email = StringField("Email")
+    submit = SubmitField("Update")
