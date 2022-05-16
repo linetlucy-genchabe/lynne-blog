2b9d0c6f6c6cb37b3c3ca1e4915737d21466f08f
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-16 14:28:59 +0300

    added unittests

---
+++ tests/test_quote.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Quote
+
+class TestQuote(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.random_quote = Quote("lynne genchabe", "software development is awesome")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.random_quote, Quote))
+
+    def test_init(self):
+        self.assertEqual(self.random_quote.author, "lynne genchabe")
+        self.assertEqual(self.random_quote.quote,"software development is awesome")
